604b722e4c111c4110c824b282cb3e50de99e5cc
Naveen <navinmachari77@gmail.com>
2026-04-12 22:19:53 +0530

    Initial Commit

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -33,7 +33,7 @@ const Footer = () => {
 
             <div className="flex flex-col gap-2 sm:text-sm max-md:text-xs py-4">
               <p className="font-semibold text-base sm:text-sm">Get in touch</p>
-              <p className="cursor-pointer hover:underline">+1-234-567-890</p>
+              <p className="cursor-pointer hover:underline">+91 12345-6789</p>
               <p className="cursor-pointer hover:underline">contact@example.com</p>
             </div>
 
