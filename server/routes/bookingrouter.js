064f85240f15de531dbebf7a987362fea109c1e3
Naveen <navinmachari77@gmail.com>
2026-04-12 16:13:53 +0530

    Initial Commit

--- server/routes/bookingrouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createBooking, getoccupiedSeats } from '../Control/Bookingscontrol.js';
-
-const bookingRouter = express.Router();
-
-bookingRouter.post('/create', createBooking);
-bookingRouter.get('/seats/:showId', getoccupiedSeats);
-
-export default bookingRouter;
--- server/routes/userrouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getfavorites, getUserbookings, updateUserfavorites } from "../Control/Usercontrol.js";
-const userRouter = express.Router();
-
-userRouter.get('/userbookings', getUserbookings);
-userRouter.post('/updatefavorites', updateUserfavorites);
-userRouter.get('/getfavorites', getfavorites);
-
-export default userRouter;
